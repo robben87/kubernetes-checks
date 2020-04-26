b1536e5dfed23a26203827b469536ad4d5e86ee3
root <root@dev-ldocflykubem01.esec.local>
2020-04-27 00:40:09 +0200

    Kubernetes Nagios Check with Pipenv and pipenv-shebang

---
+++ README.md
@@ -0,0 +1,60 @@
+yum install python3-pip.noarch
+pip3 install pipenv-shebang   #Link --> https://github.com/laktak/pipenv-shebang
+pip3 install --user pipenv
+
+export PATH=~/.local/bin:$PATH
+
+cd in directory script
+
+pipenv install
+
+Output Similar to:
+
+    Creating a virtualenv for this project…
+    Pipfile: /root/kubernetes-check/Pipfile
+    Using /usr/bin/python3 (3.6.8) to create virtualenv…
+    ⠴ Creating virtual environment...created virtual environment CPython3.6.8.final.0-64 in 266ms
+      creator CPython3Posix(dest=/root/.local/share/virtualenvs/kubernetes-check-GK_gVvOF, clear=False, global=False)
+      seeder FromAppData(download=False, pip=latest, setuptools=latest, wheel=latest, via=copy, app_data_dir=/root/.local/share/virtualenv/seed-app-data/v1.0.1)
+      activators BashActivator,CShellActivator,FishActivator,PowerShellActivator,PythonActivator,XonshActivator
+    
+    ✔ Successfully created virtual environment! 
+    Virtualenv location: /root/.local/share/virtualenvs/kubernetes-check-GK_gVvOF
+    Creating a Pipfile for this project…
+    Installing kubernetes…
+    Adding kubernetes to Pipfile's [packages]…
+    ✔ Installation Succeeded 
+    Installing numpy…
+    Adding numpy to Pipfile's [packages]…
+    ✔ Installation Succeeded 
+    Installing pandas…
+    Adding pandas to Pipfile's [packages]…
+    ✔ Installation Succeeded 
+    Pipfile.lock not found, creating…
+    Locking [dev-packages] dependencies…
+    Locking [packages] dependencies…
+    ✔ Success! 
+    Updated Pipfile.lock (778713)!
+    Installing dependencies from Pipfile.lock (778713)…
+         ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 20/20 — 00:00:09
+    To activate this project's virtualenv, run pipenv shell.
+    Alternatively, run a command inside the virtualenv with pipenv run.
+	
+
+Usage:
+    [root@machine ~]# ~/kubernetes-check/nagios_check.py
+    usage: nagios_check.py [-h] {node,pod} ...
+    
+    positional arguments:
+      {node,pod}
+        node      check_nodes status)
+        pod       check_pods status
+    
+    optional arguments:
+      -h, --help  show this help message and exit
+
+
+
+    [root@machine ~]# ~/kubernetes-check/nagios_check.py pod --config config
+	
+    [root@machine ~]# ~/kubernetes-check/nagios_check.py node --config config
---
+++ nagios_check.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env pipenv-shebang
+import socket
+import sys
+import numpy as np
+import pandas as pd
+import argparse
+import os
+from kubernetes import client, config
+#config.load_kube_config()
+
+def load_configuration(configuration):
+    config_dir = "~/.kube/"
+    config_name= args.configfile
+    kubeconfig = os.path.join(config_dir,config_name)
+    print(kubeconfig)
+    config.load_kube_config(config_file=kubeconfig)
+
+
+def check_node():
+    configuration = args.configfile
+    load_configuration(configuration)
+    v1 = client.CoreV1Api()
+    list_nodes = v1.list_node()
+    data = np.array([['','Name','Type','Status','Reason','Message']])
+    
+    for node in list_nodes.items:
+        name_list=node.metadata.name
+        status_node = v1.read_node_status(name=name_list)
+        condition=status_node.status.conditions
+        for value in condition:
+            name_node=status_node.metadata.name
+            type_obj=value.type
+            message=value.message
+            status=value.status
+            reason=value.reason
+            if str(status) != "False" and str(type_obj) != "Ready" and  str(type_obj) != "OutOfDisk":
+                        data = np.append(data,[['',name_node,type_obj,status,reason,message]],axis=0)
+            df = pd.DataFrame(data=data[1:,0:],index=data[1:,0],columns=data[0,0:])
+            output = df.to_string(justify='left') 
+    if df.empty:
+        print("NULLA")
+        sys.exit(0);    
+    else:
+        print(output)
+        sys.exit(1);  
+
+
+def check_pod():
+    configuration = args.configfile
+    load_configuration(configuration)
+    v1=client.CoreV1Api()
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    data = np.array([['','NAMESPACE','NAME','STATUS','HOST','MESSAGE','REASON'],['','','','','','','']])
+    for pod in ret.items:
+        if str(pod.status.phase) != "Running":
+            if str(pod.status.phase) != "Succeeded":
+                if str(pod.status.host_ip) == "None":
+                    hostname = ""
+                    pod.status.host_ip = ""
+                else:
+                    host_lookup = socket.gethostbyaddr(str(pod.status.host_ip))
+                    hostname = host_lookup[0]
+                #print("%s\t%s\t%s\t%s\t%s" % (pod.metadata.namespace, pod.metadata.name, pod.status.phase,hostname,pod.status.host_ip))
+                if pod.status.message is None and pod.status.reason is None and pod.status.nominated_node_name is None:
+                    pod.status.message = ""
+                    pod.status.reason  = ""
+                data = np.append(data,[['',pod.metadata.namespace, pod.metadata.name, pod.status.phase,hostname,pod.status.message, pod.status.reason]],axis=0)
+                df = pd.DataFrame(data=data[1:,0:],index=data[1:,0],columns=data[0,0:])
+                output = df.to_string(justify='center') 
+    if df.empty:
+        print("NULLA POD")
+        sys.exit(0);    
+    else:
+        print(output)
+        sys.exit(1);
+
+if __name__=='__main__':
+
+    parser=argparse.ArgumentParser()
+    subparsers=parser.add_subparsers()
+    
+    #createtheparserforthe"node"command
+    parser_node=subparsers.add_parser("node",help="check_nodes status)")
+    parser_node.add_argument("-conf","--configfile",help="Specify kubernetes config file name to load (default path is ~/.kube)",default="config")
+    parser_node.set_defaults(func=check_node)
+    #createtheparserforthe"pod"command
+    parser_pod=subparsers.add_parser("pod",help="check_pods status")
+    parser_pod.add_argument("-conf","--configfile",help="Specify kubernetes config file name to load (default path is ~/.kube)",default="config")
+    parser_pod.set_defaults(func=check_pod)
+
+    if len(sys.argv[1:])==0:
+        parser.print_help()
+    args=parser.parse_args()                                                               
+
+    if "node" in sys.argv:
+        check_node()
+    elif "pod" in sys.argv:
+        check_pod()
+    else:
+        print("")
+
