c67ad99b10dd138b8abc8bda1a9d12453526d0e6
root <root@ldocflykubem01.esec.local>
2020-04-28 16:26:44 +0200

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo command[check_pods]=/usr/lib64/nagios/plugins/kubernetes-checks/nagios_check.sh pod >> /etc/nagios/nrpe.cfg 
-command[check_nodes]=/usr/lib64/nagios/plugins/kubernetes-checks/nagios_check.sh node    >> /etc/nagios/nrpe.cfg
+echo command[check_nodes]=/usr/lib64/nagios/plugins/kubernetes-checks/nagios_check.sh node    >> /etc/nagios/nrpe.cfg
 
 
 systemctl restart nrpe
 
-cat /etc/nagios/nrpe.cfg
+tail -15  /etc/nagios/nrpe.cfg
+
+systemctl status nrpe
